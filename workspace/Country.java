c5a5cd59a6a51020043b2b870a49a04bddea8d85
yamanalosh <yamanalosh9@gmail.com>
2025-01-16 16:49:03 +0000

    submission

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,14 +1,57 @@
+// Name: Yaman Alosh
+// Date: 1/16/25
+// Makes country objects as well as their capital, language, and image file.
+// Enables you to do certain things with the countries.
 public class Country
 {
   // add private instance variables for the name, capital, language, and image file.
-
+  private String name;
+  private String capital;
+  private String language;
+  private String imageFile;
   // add constructors
-
+  public Country() {
+    name = "China";
+    capital = "Beijing";
+    language = "Chinese";
+    imageFile = "China.jpg";
+  }
+  public Country(String countryName, String countryCapital, String countryLanguage, String countryImage) {
+    name = countryName;
+    capital = countryCapital;
+    language = countryLanguage;
+    imageFile = countryImage;
+  }
   // Write accessor/get methods for each instance variable that returns it.
+  public String getName() {
+    return name;
+  }
+  public String getCapital() {
+    return capital;
+  }
+  public String getLanguage() {
+    return language;
+  }
+  public String getImageFile() {
+    return imageFile;
+  }
+  public void setName(String newName) {
+    name = newName;
+  }
+  public void setCapital(String newCapital) {
+    capital = newCapital;
+  }
+  public void setLanguage(String newLanguage) {
+    language = newLanguage;
+  }
+  public void setImageFile(String newImageFile) {
+    imageFile = newImageFile;
+  }
 
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
+  public String toString() {
+    return "The capital of " + name + " is " + capital + " and its language is " + language + ".";
+  }
+
  
-
-
-  
 }
--- workspace/Main.java
+++ workspace/Main.java
@@ -1,4 +1,7 @@
-import java.awt.*;
+// Name: Yaman Alosh
+// Date: 1/16/25
+// Enables you to do certain things with a list of countries and their capital, language and image.
+import java.awt.FlowLayout;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
@@ -17,6 +20,7 @@ public class Main
   private ImageIcon img;
   private JLabel imageLabel;
   private JLabel outputLabel;
+  private JTextField input;
   
   public static void main(String[] args) {
     // Create the GUI
@@ -28,6 +32,7 @@ public class Main
   /* loadCountries() reads in the data from the countries-data.csv file and fills in the countryArray with data. You need to add the loop that reads in the country data into the array. */
   public void loadCountries() 
   {
+    
     // Open the data file - do not change
     File file = new File("/workspaces/Countries/workspace/countries-data.csv");
     Scanner scan = null;
@@ -37,40 +42,52 @@ public class Main
         System.out.println("File not found");     
     }
     
-    // Write a for loop that goes through the countryArray.
-    // for(int i ....) {
-    // Do the following inside the loop
-      String input = scan.nextLine();
-      String[] data = input.split(",");
-      System.out.println("Read in " + data[0]);
-      // inside the loop, create a new Country using your constructor with 4 arguments and pass in data[0], data[1], data[2], data[3] as arguments.
-     // inside the loop, set countryArray[i] to the created Country object
-     
-    
+    for(int i = 0; i < countryArray.length; i++)
+    {
+       // Do the following inside the loop. The first 3 lines are written for you.
+       String input = scan.nextLine(); // read in a line from the file
+       String[] data = input.split(","); // split it into a data array on the commas
+       System.out.println("Read in " + data[0]);
+      
+        // Complete this part
+       // create a new Country object using your constructor with 4 arguments and pass in data[0], data[1], data[2], data[3] as arguments.
+       Country newCountry = new Country(data[0], data[1], data[2], data[3]);
+      // set countryArray[i] to the created Country object
+      countryArray[i] = newCountry;
+   }
+  
   }
 
   /* showCountry() will show the image associated with the current country. It should get the country at index from the countryArray. It should use its get method to get its image file name and use the code below to put the image in the GUI.
   */
   public void showCountry() {
     // Get the country at index from countryArray
-    
+    Country countryGotten = countryArray[index];
     // Use its get method to get the its image file name and save it into imagefile variable below instead of worldmap.jpg.
-    String imagefile = "worldmap.jpg";
+    String imagefile = countryArray[index].getImageFile();
     // Use the following code to create an new Image Icon and put it into the GUI
     img = new ImageIcon("/workspaces/Countries/workspace/"+imagefile);
     imageLabel.setIcon(img);
   }
-  
   /* nextButton should increment index. If the index is greater than 9, reset it back to 0. Clear the outputLabel to empty string using setText, and call showCountry();*/
   public void nextButtonClick()
   {
-    
+    index += 1;
+    if (index > 9) {
+      index = 0;
+    }
+    outputLabel.setText("");
+    showCountry();
+
   }
   
   /* reviewButton should get the country at index from the countryArray, call its toString() method and save the result, print it out with System.out.println and as an argument to outputLabel.setText( text to print out ); */
   public void reviewButtonClick()
   {
-     
+     Country countryGottenAgain = countryArray[index];
+     String countryString = countryGottenAgain.toString();
+     System.out.println(countryString);
+     outputLabel.setText(countryString);
   }
 
   /* quizButton should clear the outputLabel (outputLabel.setText to empty string), get the country at index from countryArray, print out a question about it like What country is this? and/or What's this country's capital?. Get the user's answer using scan.nextLine() and check if it is equal to the country's data using its get methods and print out correct or incorrect.
@@ -78,11 +95,19 @@ public class Main
   public void quizButtonClick()
   {
     Scanner scan = new Scanner(System.in); 
-    
-    
+    outputLabel.setText("");
+    outputLabel.setText("What's the capital?");
     
   }
 
+  public void goButtonClicked(){
+    if (input.getText().equals(countryArray[index].getCapital())) {
+      outputLabel.setText("Correct!");
+    }
+    else {
+      outputLabel.setText("Wrong.");
+    }
+  }
 
 
 
@@ -96,9 +121,11 @@ public Main() {
         JButton reviewButton = new JButton("Review");
         JButton quizButton = new JButton("Quiz");
         JButton newButton = new JButton("Next");
+        JButton goButton = new JButton("Go!");
         jFrame.add(reviewButton);
         jFrame.add(quizButton);
         jFrame.add(newButton);
+        jFrame.add(goButton);
         
         // create a new image icon
         img = new ImageIcon("worldmap.jpg");
@@ -108,6 +135,9 @@ public Main() {
         outputLabel = new JLabel();
         jFrame.add(imageLabel);
         jFrame.add(outputLabel);
+        input = new JTextField(20);
+        jFrame.add(input);
+
         jFrame.setVisible(true);
         // add event listener for button click
         reviewButton.addActionListener(new ActionListener() {
@@ -116,6 +146,15 @@ public Main() {
       reviewButtonClick();
     }
         });
+
+
+        goButton.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) 
+          {
+            goButtonClicked();
+          }
+              });
+        
     quizButton.addActionListener(new ActionListener() {
     public void actionPerformed(ActionEvent e) 
     {
